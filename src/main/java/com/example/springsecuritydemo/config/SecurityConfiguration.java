4d2a5bf0753101ce234d71f0c48bcaca2ddf687e
taras <tarasuchok@ukr.net>
2025-01-29 16:11:37 +0200

    add docker compose

--- src/main/java/com/example/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
-                        .requestMatchers("/api/token", "/api/register").permitAll()
+                        .requestMatchers("/api/token", "/api/register", "/registration-confirm").permitAll()
                         .anyRequest().authenticated()
                         .and()
                         .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
